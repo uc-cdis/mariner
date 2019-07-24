859125f9e8984d0e81adf1fd0c2357b492cafed8
Matt Garvin <mattgarvin@Matts-MacBook-Pro.local.cdis>
2019-07-24 16:09:30 -0500

    Troubleshoot bucket mounting.

--- Docker/s3Sidecar/s3sidecarDockerrun.sh
+++ Docker/s3Sidecar/s3sidecarDockerrun.sh
@@ -9,17 +9,21 @@
 export AWS_ACCESS_KEY_ID=$(echo $AWSCREDS | jq .id | tr -d '"')
 export AWS_SECRET_ACCESS_KEY=$(echo $AWSCREDS | jq .secret | tr -d '"')
 
-echo "mounting prefix $S3PREFIX"
-goofys workflow-engine-garvin:$S3PREFIX /data
+# echo "mounting prefix $S3PREFIX"
+# goofys workflow-engine-garvin:$S3PREFIX /data
 # <- common to engine and task
 
 # conditional here
 if [ $MARINER_COMPONENT == "ENGINE" ]; then
   echo "setting up for the engine.."
+  echo "mounting prefix $S3PREFIX"
+  goofys workflow-engine-garvin:$S3PREFIX /data
   echo $WORKFLOW_REQUEST > /data/request.json
   echo "successfully wrote workflow request to /data/request.json"
 else # $MARINER_COMPONENT is "TASK"
   echo "setting up for a task.."
+  echo "mounting prefix $S3PREFIX"
+  goofys -f workflow-engine-garvin:$S3PREFIX /data
   echo $TOOL_COMMAND > $TOOL_WORKING_DIR\run.sh # this might be a problematic way of writing/passing the command - quotations and spaces/breaks preserved or not, etc
   echo "successfully wrote tool command to $TOOL_WORKING_DIR\run.sh"
 fi
