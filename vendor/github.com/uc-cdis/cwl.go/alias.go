5d48e88eec38f5ee0285160eb29245233dfd8c90
mattgarvin1 <mattgarvin1@gmail.com>
2019-09-13 15:18:09 -0500

    Really fix dependencies.

--- vendor/github.com/uc-cdis/cwl.go/alias.go
+++ vendor/github.com/uc-cdis/cwl.go/alias.go
@@ -4,10 +4,12 @@ import "strings"
 
 // Alias represents an expression with "$(xxx.yyy)"
 type Alias struct {
-	string
+	String string
 }
+
+// NOTE: this is gonna be a problem!
 
 // Key extract the exact (and flattened) name of an expression.
 func (a *Alias) Key() string {
-	return strings.Trim(a.string, "$()")
+	return strings.Trim(a.String, "$()")
 }
