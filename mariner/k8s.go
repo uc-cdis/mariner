f284b05ec90e1ddaca409df0130646770cb43e91
mattgarvin1 <mattgarvin1@gmail.com>
2019-11-06 15:13:33 -0600

    Fix engine wait loop.

--- mariner/k8s.go
+++ mariner/k8s.go
@@ -201,7 +201,7 @@ func engineArgs(runID string) []string {
 	args := []string{
 		"-c",
 		fmt.Sprintf(`
-    while [[ ! -f /$marinerEngine_WORKSPACE/workflowRuns/$RUN_ID/request.json ]]; do
+    while [[ ! -f /$ENGINE_WORKSPACE/workflowRuns/$RUN_ID/request.json ]]; do
       echo "Waiting for mariner-engine-sidecar to finish setting up..";
       sleep 1
     done
