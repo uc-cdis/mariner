8b30e09db2b9220b0fc4a012ff9fbdb1c3d957a2
mattgarvin1 <mattgarvin1@gmail.com>
2020-01-03 16:53:32 -0600

    Fill out core object fields.

--- wftool/schema.go
+++ wftool/schema.go
@@ -2,6 +2,8 @@ package wftool
 
 import ()
 
+// right now pretty much just writing out the CWL spec in Go types
+
 // WorkflowJSON is the JSON representation of a CWL workflow
 type WorkflowJSON struct {
 	Graph WorkflowGraph
@@ -10,16 +12,52 @@ type WorkflowJSON struct {
 // WorkflowGraph contains all the CWLObjects of the workflow
 type WorkflowGraph []CWLObject
 
+// as much as possible, don't repeat fields among structs
+// make basic, atomic structs, and embed into other structs as needed
+
 // CWLObject represents a workflow, expressiontool, commandlinetool, ...
+// TODO
 type CWLObject interface {
 	// some method
 }
 
-// CWLWorkflow ..
-type CWLWorkflow struct{}
+// CoreFields are common to workflow, expressiontool, commandlinetool, ...
+type CoreFields struct {
+	ObjectMeta
+	ReqsAndHints
+	Class      string
+	CWLVersion string
+}
 
-// CWLCommandLineTool ..
-type CWLCommandLineTool struct{}
+// ReqsAndHints ..
+type ReqsAndHints struct {
+	Requirements []Requirement
+	Hints        []Hint
+}
 
-// CWLExpressionTool ..
-type CWLExpressionTool struct{}
+// ObjectMeta ..
+type ObjectMeta struct {
+	ID    string
+	Label string
+	Doc   string
+}
+
+// Workflow ..
+// TODO
+type Workflow struct{}
+
+// CommandLineTool ..
+// TODO
+type CommandLineTool struct{}
+
+// ExpressionTool ..
+// TODO
+type ExpressionTool struct{}
+
+// Requirement ..
+// TODO
+type Requirement struct{}
+
+// Hint ..
+// TODO
+type Hint struct{}
