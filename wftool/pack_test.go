0b2d20c2e451b79916fae01c56eede5bb82261aa
mattgarvin1 <mattgarvin1@gmail.com>
2020-01-13 14:28:34 -0600

    add workflow cwl to test

--- wftool/pack_test.go
+++ wftool/pack_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 )
 
-var input = `
+var tool = `
 #!/usr/bin/env cwl-runner
 
 cwlVersion: v1.0
@@ -52,6 +52,51 @@ arguments:
     valueFrom: 'touchFiles.sh'
 `
 
+var workflow = `
+#!/usr/bin/env cwl-runner
+
+cwlVersion: v1.0
+
+class: Workflow
+
+requirements:
+  - class: InlineJavascriptRequirement
+
+inputs:
+    commons_file_1: File
+    commons_file_2: File
+    user_file: File
+
+outputs:
+    output_files:
+        type: File[]
+        outputSource: read_from_all/output_files
+
+steps:
+    read_from_commons_and_user:
+        run: ./read_from_commons_and_user.cwl
+        in:
+            commons_file_1: commons_file_1
+            commons_file_2: commons_file_2
+            user_file: user_file
+        out: [processed_file_1, processed_file_2, processed_file_3]
+
+    read_from_all:
+        run: ./read_from_all.cwl
+        in:
+            processed_file_1: 
+                source: read_from_commons_and_user/processed_file_1
+            processed_file_2: 
+                source: read_from_commons_and_user/processed_file_2
+            processed_file_3:
+                source: read_from_commons_and_user/processed_file_3
+            unprocessed_file_1: commons_file_1
+            unprocessed_file_2: commons_file_2
+            unprocessed_file_3: user_file
+        out: [ output_files ]
+`
+
 func TestPack(t *testing.T) {
-	Pack([]byte(input))
+	Pack([]byte(tool))
+	Pack([]byte(workflow))
 }
