eb85e2025a3e1a0ba16b2e6dc5ed4d8dcddeb625
mattgarvin1 <mattgarvin1@gmail.com>
2020-01-15 17:10:03 -0600

    add expressiontool test

--- wftool/pack_test.go
+++ wftool/pack_test.go
@@ -96,7 +96,39 @@ steps:
         out: [ output_files ]
 `
 
+var expressiontool = `
+#!/usr/bin/env cwl-runner
+
+cwlVersion: v1.0
+
+requirements:
+  - class: InlineJavascriptRequirement
+
+class: ExpressionTool
+
+inputs:
+  file_array:
+    type:
+      type: array
+      items: ['null', 'File']
+
+outputs:
+  output: File[]
+
+expression: |
+  ${
+    var trueFile = [];
+    for (var i = 0; i < inputs.file_array.length; i++){
+      if (inputs.file_array[i] != null){
+        trueFile.push(inputs.file_array[i])
+      }
+    };
+    return {'output': trueFile};
+  }
+`
+
 func TestPack(t *testing.T) {
-	Pack([]byte(tool), "#read_from_all.cwl")
+	// Pack([]byte(tool), "#read_from_all.cwl")
 	// Pack([]byte(workflow), "#main")
+	Pack([]byte(expressiontool), "#expressiontool_test.cwl")
 }
