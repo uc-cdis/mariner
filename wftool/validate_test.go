f5a43e9564774f52b659ff5ff70282851f3eff5e
mattgarvin1 <mattgarvin1@gmail.com>
2020-01-27 18:27:06 -0600

    cleanup test output

--- wftool/validate_test.go
+++ wftool/validate_test.go
@@ -1,23 +1,20 @@
 package main
 
 import (
-	"fmt"
 	"testing"
 )
 
 func TestValidate(t *testing.T) {
-	// wd, _ := os.Getwd()
-	// fmt.Println("wd: ", wd)
 	l := []string{
 		noInputTargetJSON,
 		userDataTargetJSON,
 	}
 	for _, f := range l {
-		valid, grievances := ValidateJSONFile(f)
-		// valid, _ := ValidateJSONFile(f)
+		// valid, grievances := ValidateJSONFile(f)
+		valid, _ := ValidateJSONFile(f)
 		if !valid {
-			fmt.Println("grievances: ")
-			printJSON(grievances)
+			// fmt.Println("grievances: ")
+			// printJSON(grievances)
 			t.Errorf("%v failed validation", f)
 		}
 	}
