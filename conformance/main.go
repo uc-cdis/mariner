dcedc8e9ee69dd9bf36bd66b2bdb7d3cf351078a
mattgarvin1 <mattgarvin1@gmail.com>
2020-02-07 13:53:46 -0600

    add core functions

--- conformance/main.go
+++ conformance/main.go
@@ -1,10 +1,40 @@
 package main
 
-import (
-	mariner "github.com/uc-cdis/mariner/mariner"
-	wftool "github.com/uc-cdis/mariner/wftool"
-)
-
 func main() {
 
+}
+
+// path to config: 		./common-workflow-language/v1.0/conformance_test_v1.0.yaml
+// path to test suite: 	./common-workflow-language/v1.0/v1.0/
+
+const (
+	config = "./common-workflow-language/v1.0/conformance_test_v1.0.yaml"
+)
+
+func runTests(apiKey string) error {
+	tok, err := token(apiKey)
+	if err != nil {
+		return err
+	}
+	suite, err := loadConfig(config)
+	if err != nil {
+		return err
+	}
+	for _, test := range suite {
+		// could make a channel to capture errors from individual tests
+		go runTest(test, tok)
+	}
+	return nil
+}
+
+func loadConfig(config string) ([]map[string]interface{}, error) {
+	return nil, nil
+}
+
+func token(apiKey string) (string, error) {
+	return "", nil
+}
+
+func runTest(test map[string]interface{}, tok string) {
+	return
 }
