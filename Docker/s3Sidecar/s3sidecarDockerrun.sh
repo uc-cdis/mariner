f8c33ee38959720bbf4c7eecae20dfc490a79a41
mattgarvin1 <mattgarvin1@gmail.com>
2020-09-24 10:54:41 -0500

    add sleep statement before unmount s3 sidecar

--- Docker/s3Sidecar/s3sidecarDockerrun.sh
+++ Docker/s3Sidecar/s3sidecarDockerrun.sh
@@ -30,7 +30,7 @@ if [ $MARINER_COMPONENT == "engine" ]; then
   echo "waiting for workflow to finish.."
   sleep 10
   while [[ ! -f /$ENGINE_WORKSPACE/workflowRuns/$RUN_ID/done ]]; do
-    sleep 10
+    sleep 5
   done
 else # $MARINER_COMPONENT is "task"
   echo "setting up for a task.."
@@ -46,9 +46,11 @@ else # $MARINER_COMPONENT is "task"
   ls $TOOL_WORKING_DIR
   sleep 10
   while [[ ! -f $TOOL_WORKING_DIR\done ]]; do
-    sleep 10
+    sleep 5
   done
 fi
+
+sleep 5
 
 # unmount engine workspace
 echo "done, unmounting goofys"
