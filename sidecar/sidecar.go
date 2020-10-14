716a8de3d0a2623e5f7852bd337c7f001a1ef391
mattgarvin1 <mattgarvin1@gmail.com>
2020-10-14 09:25:40 -0500

    build out first few main fns

--- sidecar/sidecar.go
+++ sidecar/sidecar.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
 
 	/*
@@ -10,11 +12,46 @@ func main() {
 		1. read 's3://<twd>/_mariner_s3_paths'
 		2. download those files from s3
 		3. signal to main to run
+
+		// here!
 		4. wait
 		5. upload output (?) files to s3
 		6. exit 0
 
 	*/
 
+	// 1. read in the target s3 paths
+	s3Paths, err := readMarinerS3Paths()
+	if err != nil {
+		fmt.Println("readMarinerS3Paths failed:", err)
+	}
+
+	// 2. download those files to the shared volume
+	err = downloadFiles(s3Paths)
+	if err != nil {
+		fmt.Println("downloadFiles failed:", err)
+	}
+
+	// 3. signal main container to run
+	err = signalTaskToRun()
+	if err != nil {
+		fmt.Println("signalTaskToRun failed:", err)
+	}
+
 	return
+}
+
+// 1. read 's3://<twd>/_mariner_s3_paths'
+func readMarinerS3Paths() ([]string, error) {
+	return nil, nil
+}
+
+// 2. batch download target s3 paths
+func downloadFiles(s3Paths []string) error {
+	return nil
+}
+
+// 3. signal to main container to run
+func signalTaskToRun() error {
+	return nil
 }
