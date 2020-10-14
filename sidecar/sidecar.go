e73f1a95198b31626873f4aa77cc1ac84a63f2b9
mattgarvin1 <mattgarvin1@gmail.com>
2020-10-14 08:49:48 -0500

    add empty new sidecar pkg

---
+++ sidecar/sidecar.go
@@ -0,0 +1,20 @@
+package main
+
+func main() {
+
+	/*
+		steps:
+
+		00. load in vars from envVars
+		0. configure the AWS interface with the creds
+		1. read 's3://<twd>/_mariner_s3_paths'
+		2. download those files from s3
+		3. signal to main to run
+		4. wait
+		5. upload output (?) files to s3
+		6. exit 0
+
+	*/
+
+	return
+}
