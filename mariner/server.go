a564f97e32a2b1254aecfdb4f6bd4b4535aa2549
cterrazas2 <cterrazas69@gmail.com>
2021-04-02 08:32:49 -0700

    grabbing host name from os env and fixing verb for string

--- mariner/server.go
+++ mariner/server.go
@@ -514,7 +514,7 @@ func (server *Server) authZ(r *http.Request) bool {
 
 // fetchRefreshToken is invoked from the server to check if a resfresh token is expired and fetches a new one if it is.
 func (server *Server) fetchRefreshToken() bool {
-	wtsPath := fmt.Sprintf("https://%d/wts/oauth2/", envVarHostname)
+	wtsPath := fmt.Sprintf("https://%s/wts/oauth2/", os.Getenv("GEN3_HOSTNAME"))
 	connectedUrl := wtsPath + "connected"
 	res, err := http.Get(connectedUrl)
 	if err != nil {
