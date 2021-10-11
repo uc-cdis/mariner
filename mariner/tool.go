001a4557fd8488f4e4fb977987c4d7de57dd96cd
Kyle Hernandez <kmhernan84@gmail.com>
2021-10-11 15:26:39 -0500

    fix(initwkdir): handle File pointer case

--- mariner/tool.go
+++ mariner/tool.go
@@ -89,6 +89,24 @@ func (engine *K8sEngine) initWorkDirReq(tool *Tool) (err error) {
 										log.Errorf("unsupported initwkdir path: %v", path)
 										return tool.Task.errorf("unsupported initwkdir path: %v", path)
 									}
+								case *File:
+									path, err := v.Path
+									if err != nil {
+										tool.Task.infof("failed to extract path from file: %v", v)
+										continue
+									}
+								        tool.Task.infof("*File - Path: %v", path)
+									switch {
+									case strings.HasPrefix(path, userPrefix):
+										trimmedPath := strings.TrimPrefix(path, userPrefix)
+										path = strings.Join([]string{"/", engineWorkspaceVolumeName, "/", trimmedPath}, "")
+										tool.Task.infof("adding initwkdir path: %v", path)
+										tool.S3Input.Paths = append(tool.S3Input.Paths, path)
+										tool.initWorkDirFiles = append(tool.initWorkDirFiles, path)
+									default:
+										log.Errorf("unsupported initwkdir path: %v", path)
+										return tool.Task.errorf("unsupported initwkdir path: %v", path)
+									}
 								default:
 									log.Errorf("unsupported initwkdir type: %T; value: %v", v, v)
 									return tool.Task.errorf("unsupported initwkdir type: %T; value: %v", v, v)
