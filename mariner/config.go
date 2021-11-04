298f5a59e69a7d84752cec129038c3547fa2bb26
UchicagoZchen138 <zchen138@uchicago.edu>
2021-11-04 09:26:43 -0500

    fix change pvc from test pvc to prod pvc

--- mariner/config.go
+++ mariner/config.go
@@ -111,7 +111,7 @@ const (
 	pathToUserRunsf   = "%v/workflowRuns/"                // fill with userID
 	pathToUserRunLogf = pathToUserRunsf + "%v/" + logFile // fill with runID
 
-	commonsDataPersistentVolumeClaimName = "nfs-commons-data-test-pvc"
+	commonsDataPersistentVolumeClaimName = "mariner-nfs-pvc"
 )
 
 var (
